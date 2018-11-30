740a0c63b6024f87346c677479ffd575a5fbc52f
Aashis Kishore <aashiskishore@gmail.com>
2018-11-30 12:13:51 +0530

    Add git command shortcuts

---
+++ shortcuts.sh
@@ -0,0 +1,15 @@
+alias gs="git status"
+
+alias glg="git log --pretty=oneline"
+
+alias ga="git add "
+
+alias gaa="git add ."
+
+alias gc="git commit -m"
+
+alias gac="git commit -a -m"
+
+alias gp="git push -u origin master"
+
+alias gpb="git push -u origin "
